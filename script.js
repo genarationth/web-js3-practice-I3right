8dd0b953b032d4f4ba204b8b27732e48bf0d5c08
Bright <108338051+I3right@users.noreply.github.com>
2023-03-13 16:25:18 +0700

    add function by arrow function

---
+++ script.js
@@ -0,0 +1,11 @@
+const add = (a,b)        => console.log(a+b);
+const substract = (a,b)  => console.log(a-b);
+const multiply = (a,b)   => console.log(a*b);
+const divide = (a,b)     => console.log(a/b);
+add(5,3);
+substract(5,3);
+multiply(5,3);
+divide(5,3);
+
+const sentence = (name,location,hobby) =>    console.log(`Hi, my name is ${name} I live in ${location} and enjoy ${hobby}`);
+sentence('bright','cnx','cycling')
